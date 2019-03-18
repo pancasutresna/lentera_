75d88cb7a2e454258ba6e1821a83339bac5209a1
Panca Sutresna <panca.sutresna@gmail.com>
2019-03-18 22:54:42 +0700

    Update react-router-dom to version 4.4.0

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
-
+import TodoList from './components/todos/TodoList';
 import TodoCreate from './components/todos/TodoCreate';
 import TodoDelete from './components/todos/TodoDelete';
 import TodoEdit from './components/todos/TodoEdit';
-import TodoList from './components/todos/TodoList';
 import TodoShow from './components/todos/TodoShow';
 import Header from './components/Header';
 
@@ -16,17 +14,20 @@ import Header from './components/Header';
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Header />
-          <Route path="/" exact component={Fib} />
-          <Route path="/todos/new" component={TodoCreate} />
-          <Route path="/todos/edit" component={TodoEdit} />
-          <Route path="/todos/delete" exact component={TodoDelete} />
-          <Route path="/todos/show" exact component={TodoShow} />
-          <Route path="/otherpage" component={OtherPage} />
-        </div>
-      </Router >
+      <div className="ui container">
+        <Router>
+          <div>
+            <Header />
+            <Route exact path="/" component={Fib} />
+            <Route path="/" component={TodoList} />
+            <Route path="/todos/new" component={TodoCreate} />
+            <Route path="/todos/edit" component={TodoEdit} />
+            <Route path="/todos/delete" exact component={TodoDelete} />
+            <Route path="/todos/show" exact component={TodoShow} />
+            <Route path="/otherpage" component={OtherPage} />
+          </div>
+        </Router>
+      </div>
     );
   }
 }
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,9 +4,10 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <div className="ui secondary pointing menu">
-            <Link to="/" className="item">Todo Lists</Link>
+            <Link to="/" className="item">LENTERA</Link>
             <div className="right menu">
-                <Link to="/" className="item">All Streams</Link>
+                <Link to="/" className="item">All Todos</Link>
+                <Link to="otherpage">Other Page</Link>
             </div>
         </div>
     );
