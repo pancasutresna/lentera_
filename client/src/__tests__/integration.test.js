286468ac2da158d214c2458c7ef145dd88ecd420
Panca Sutresna <panca.sutresna@gmail.com>
2019-06-16 23:30:08 +0700

    updating comment box test

---
+++ client/src/__tests__/integration.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {mount} from 'enzyme';
+import moxios from 'moxios';
+import Root from 'Root';
+import App from 'components/App';
+
+
+beforeEach(() => {
+    moxios.install();
+    moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
+        status: 200,
+        response: [{name: 'Fetched #1'}, {name: 'Fetched #2'}]
+    });
+});
+
+afterEach(() => {
+    moxios.uninstall();
+});
+
+it('can fetch a list of comments and display then', (done) => {
+    // Attemp to render the *entire* app
+    const wrapped = mount(
+        <Root>
+            <App />
+        </Root>
+    );
+
+    wrapped.find('.fetch-comments').simulate('click');
+
+    // introduce a tiny little pause
+    setTimeout(() => {
+        wrapped.update();
+        expect(wrapped.find('li').length).toEqual(2);
+        done();
+        wrapped.unmount();
+    }, 100);
+    
+});
